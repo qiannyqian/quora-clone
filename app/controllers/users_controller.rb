00501a2ede2600dda1b63ad246c866320dcc024c
Hui Qian Liang <liang.huiqian@gmail.com>
2017-09-11 11:16:04 +0800

    Added questions and answers to db

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,14 +20,23 @@ get '/login' do
 end
 
 post '/login' do
-  user = User.find_by_email(params[:user][:email])
-  #byebug
-  if user.authenticate(params[:user][:password])
-    session[:user_id] = user.id
+  login = User.login(email, password)
+
+  # user = User.find_by_email(params[:user][:email])
+  # #byebug
+  # if user.authenticate(params[:user][:password]) # User.login(email, password)
+  #   session[:user_id] = user.id
+  #   redirect '/dashboard'
+  # else
+  #   redirect '/failure'
+  # end
+
+  if login
     redirect '/dashboard'
   else
     redirect '/failure'
   end
+  
 end
 
 get '/logout' do
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ActiveRecord::Base
+  has_many :questions
+  
   has_secure_password
 
   validates :username, presence: true
   validates :email, presence: true, uniqueness: true
+
+  def self.login(email, password)
+    user = User.find_by_email(params[:user][:email])
+    user.authenticate(params[:user][:password])
+    session[:user_id] = user.id
+  end
 
 end
